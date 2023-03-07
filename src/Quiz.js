056d2a52434a771881eccc10969e59b48ede6c73
Rex <proandceo@gmail.com>
2023-03-07 16:53:02 +1000

    update

---
+++ src/Quiz.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+let quizData = require("./quiz_data.json");
+
+class Quize extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quiz_position: 1,
+    };
+  }
+  render() {
+    <div>Quiz</div>;
+  }
+}
+
+export default Quiz;
